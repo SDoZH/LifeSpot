a4a702e198859bb46fde8d21597a33fdde5d5229
SDoZH <105928843+SDoZH@users.noreply.github.com>
2023-01-12 03:55:21 +0300

    Add files via upload

---
+++ LifeSpot/LifeSpot/Static/JS/index.js
@@ -0,0 +1,82 @@
+﻿let sessionLog = function logSession(session) {
+    // Вывод в консоль лог
+    for (let result of session) {
+        console.log(result)
+    }
+}
+
+setTimeout(() =>
+    alert("Нравится LifeSpot? " + '\n' + "Подпишитесь на наш Instagram @lifespot999!"),
+    30000); 
+
+let checker = function (newVisit) {
+    if (window.sessionStorage.getItem("userAge") >= 18) {
+        // Добавим проверку на первое посещение, чтобы не показывать приветствие
+        // лишний раз
+        if (newVisit) {
+            alert("Приветствуем на LifeSpot! " + '\n' + "Текущее время: " + new Date().toLocaleString());
+        }
+    }
+    else {
+        alert("Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены");
+        window.location.href = "http://www.google.com"
+    }
+}
+
+let logger = function () {
+    console.log('Начало сессии: ' + window.sessionStorage.getItem("startDate"))
+    console.log('Даныне клиента: ' + window.sessionStorage.getItem("userAgent"))
+    console.log('Возраст пользователя: ' + window.sessionStorage.getItem("userAge"))
+}
+
+
+function handleSession(logger, checker) {
+
+    // Проверяем дату захода и проставляем, если новый визит
+    if (window.sessionStorage.getItem("startDate") == null) {
+        window.sessionStorage.setItem("startDate", new Date().toLocaleString())
+    }
+
+    // Проверяем userAgent и проставляем, если новый визит
+    if (window.sessionStorage.getItem("userAgent") == null) {
+        window.sessionStorage.setItem("userAgent", window.navigator.userAgent)
+    }
+
+    // Проверяем возраст и проставляем, если новый визит
+    if (window.sessionStorage.getItem("userAge") == null) {
+        let input = prompt("Пожалуйста, введите ваш возраст?");
+        window.sessionStorage.setItem("userAge", input)
+
+        /* Возраст отсутствовал в sessionStorage. Значит, это первый визит пользователя, и
+         при прохождении проверки на возраст он увидит приветствие*/
+        checker(true)
+    } else {
+
+        /* Пользователь заходит не первый раз, приветствие не показываем. */
+        checker(false)
+    }
+
+    /* Вызываем переданную в качестве колл-бэка функцию логирования.
+        передавать в качестве коллбека не обязательно, можно вызвать и напрямую, но мы добавили для повторения.
+    */
+    logger()
+}
+
+
+function filterContent(userInput) { // Принимаем пользовательский ввод в качестве параметра.
+    // Находим контейнеры с видео, которые необходимо фильтровать
+    let elements = document.getElementsByClassName('video-container');
+    // Пробегаемся по контейнерам
+    for (let i = 0; i <= elements.length; i++) {
+        // Вытаскиваем текст описания видео, которое необходимо отфильтровать
+        let videoText = elements[i].querySelector(".video-title").innerText;
+        // Выполняем фильтрацию, сравнивая значения в нижнем регистре
+        if (!videoText.toLowerCase().includes(userInput.toLowerCase())) {
+            // Скрываем неподходящие
+            elements[i].style.display = 'none';
+        } else {
+            // Показываем подходящие
+            elements[i].style.display = 'inline-block';
+        }
+    }
+}
---
+++ LifeSpot/LifeSpot/Static/JS/testing.js
@@ -0,0 +1,11 @@
+﻿alert("Javascript для страницы 'о проекте' подключен!")
+
+const saveInput = function () {
+    // Вытащим значение текстового поля (как у нас уже делается при фильтрации)
+    let currentInput = document.getElementsByTagName('input')[0].value.toLowerCase();
+    // Покажем окно с прошлым и новым значением
+    alert('Последний ввод: ' + this.lastInput /* равноценно window.lastInput, так как мы работаем в контексте браузера */
+        + '\n' + 'Текущий ввод: ' + currentInput);
+    // Сохраним новое значение в контекст
+    this.lastInput = currentInput;
+}
